8c21a65c2c5449805e51e829757d6ab277195d8d
hianshul07 <hianshul07@gmail.com>
2024-11-18 19:11:07 +0530

    done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,14 +18,20 @@ const Card = () => {
 			<Navbar />
 			<Destination
 				image="/src/assets/Fuji.png"
+				destination="japan"
+				date="12 Jan, 2023 - 24 Jan, 2023"
 				description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
 			/>
 			<Destination
 				image="/src/assets/Sydney.png"
+				destination="australia"
+				date="27 May, 2023 - 8 Jun, 2023"
 				description="The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
 			/>
 			<Destination
-				imgae="/src/assets/Water.png"
+				image="/src/assets/Norway.png"
+				destination="norway"
+				date="01 Oct, 2024 - 18 Nov, 2024"
 				description="The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
 			/>
 			{/* {destinations} */}
--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,9 +1,8 @@
 import pin from '../assets/pin.svg';
 import Sydney from '../assets/Sydney.png';
 import Fuji from '../assets/Fuji.png';
-import water from '../assets/water.png';
 
-const Destination = ({ image, description }) => {
+const Destination = ({ image, description, destination, date }) => {
 	return (
 		<div className="flex items-center  p-10  border-b">
 			{/* <div id='left' className=''> */}
@@ -12,11 +11,11 @@ const Destination = ({ image, description }) => {
 			<div id="right" className="px-5">
 				<div id="location" className="flex items-center">
 					<img src={pin} alt="" className="h-4 mr-1" />
-					<h3 className=" tracking-[0.17em] text-xs mr-3">JAPAN</h3>
+					<h3 className=" tracking-[0.17em] text-xs mr-3 uppercase">{destination}</h3>
 					<p className="text-[#918E9B] underline">View on Google Maps</p>
 				</div>
 				<h2 className="font-bold text-[#2B283A] text-2xl mb-4">Mount Fuji</h2>
-				<p className="font-bold text-xs mb-2">12 Jan, 2023 - 24 Jan, 2023</p>
+				<p className="font-bold text-xs mb-2">{date}</p>
 				<p className="text-xs text-[#2B283A]">
 					{description}
 				</p>
